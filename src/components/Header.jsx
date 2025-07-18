5d868f174c458c44b811efcfdfbba60489f95441
Phan Dong <duydongphan95@gmail.com>
2025-07-18 09:54:44 +0700

    them hinh anh 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <img src="./images/logo2.png" alt="Logo" className="w-10 h-10" />
+            <img src="./images/logo2.png" alt="Logo" className="w-12 h-12" />
             {/* <Leaf className="w-8 h-8 text-amber-600" /> */}
             <h1 className="text-2xl font-bold text-gray-800">Sâm Premium</h1>
           </div>
